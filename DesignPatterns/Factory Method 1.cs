882f1cfbf2bf9f8e4c0e28f0536883ee6d5d3737
Emi0689 <emiliofilotti@gmail.com>
2025-04-27 21:22:29 -0300

    Factory method

---
+++ DesignPatterns/Factory Method 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public interface IVehicle
+    {
+        void Drive();
+    }
+
+    public class Car : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Draving a car 🚗");
+        }
+    }
+
+    public class Bike : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Draving a bike 🚲");
+        }
+    }
+
+    public static class VehicleFactory
+    {
+        public static IVehicle CreateVehicle(string type)
+        {
+            if (type == "Car")
+                return new Car();
+            else if (type == "Bike")
+                return new Bike();
+            else
+                throw new ArgumentException("Type of vehicle unknown.");
+        }
+    }
+
+    class ProgramFactory1
+    {
+        static void Main(string[] args)
+        {
+            IVehicle myVehicle = VehicleFactory.CreateVehicle("Car");
+            myVehicle.Drive();
+
+            IVehicle mySecondVehicle = VehicleFactory.CreateVehicle("Bike");
+            mySecondVehicle.Drive();
+        }
+    }
+}
